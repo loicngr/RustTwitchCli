056fde4d81dac2bc55cfe351e0ff540d6c8dadc9
loicngr <pro.nogierloic@gmail.com>
2020-02-21 10:17:35 +0100

    ADD - topgames and topgame command

--- README.md
+++ README.md
@@ -1,19 +1,33 @@
 # Available commands
 
 ### Env vars (.env)
+
 #### Add this lines in your .env file :
+
     TWITCH_CLIENT_ID=
     TWITCH_CLIENT_SECRET=
     TWITCH_CLIENT_TOKEN=
 
 ### Generate Token
+
 - > cargo run token
 
 ### Get User by id
+
 - > cargon run info-user=53380605
 
 ### Get user Stream by id
+
 - > cargo run isonlive-user=147337430
 
 ### Get an user id by user login
+
 - > cargo run uid=username
+
+### Get Top Games
+
+- > cargo run topgames
+
+### Get Top Game
+
+- > cargo run topgame
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+extern crate dotenv;
 extern crate reqwest;
 extern crate serde_json;
-extern crate dotenv;
 
 use dotenv::dotenv;
 use serde::{Deserialize, Serialize};
@@ -12,7 +12,7 @@ struct Cli {
     params: Vec<String>,
     twitch_client_id: String,
     twitch_client_secret: String,
-    twitch_client_token: String
+    twitch_client_token: String,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -26,7 +26,7 @@ struct User {
     description: String,
     view_count: u32,
     offline_image_url: String,
-    profile_image_url: String
+    profile_image_url: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -41,99 +41,137 @@ struct Stream {
     viewer_count: u32,
     started_at: String,
     language: String,
-    thumbnail_url: String
+    thumbnail_url: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct TopGames {
+    id: String,
+    name: String,
+    box_art_url: String,
 }
 
 impl Cli {
-    async fn get_token( &self ) -> Result<String, reqwest::Error> {
+    async fn get_token(&self) -> Result<String, reqwest::Error> {
         let client = reqwest::Client::new();
         let resp = client.post(format!("https://id.twitch.tv/oauth2/token?client_id={}&client_secret={}&grant_type=client_credentials", self.twitch_client_id, self.twitch_client_secret).as_str())
             .send().await?;
-    
         let text = resp.text().await?;
         Ok(text)
     }
-    fn remove_data_scope( &self, text: &str ) -> Result<String, serde_json::Error> {
+    fn remove_data_scope(&self, text: &str) -> Result<String, serde_json::Error> {
         let deserialized: serde_json::Value = serde_json::from_str(&text).unwrap();
         let data = deserialized.get("data").unwrap();
         let serialized = serde_json::to_string(&data).unwrap();
         Ok(serialized)
     }
-    async fn get_stream_user( &self, uid: u32 ) -> Result<Vec<Stream>, reqwest::Error> {
+    async fn get_stream_user(&self, uid: u32) -> Result<Vec<Stream>, reqwest::Error> {
         let client = reqwest::Client::new();
-        let resp = client.get(format!("https://api.twitch.tv/helix/streams?user_id={}", &uid).as_str())
-                .header("Client-ID", self.twitch_client_id.as_str())
-                .send().await?;
+        let resp = client
+            .get(format!("https://api.twitch.tv/helix/streams?user_id={}", &uid).as_str())
+            .header("Client-ID", self.twitch_client_id.as_str())
+            .send()
+            .await?;
         let text = resp.text().await?;
         let data = self.remove_data_scope(&text.as_str()).unwrap();
         let stream: Vec<Stream> = serde_json::from_str(&data).unwrap();
         Ok(stream)
     }
-    async fn get_user_by_login( &self, login: String ) -> Result<Vec<User>, reqwest::Error> {
+    async fn get_user_by_login(&self, login: String) -> Result<Vec<User>, reqwest::Error> {
         let client = reqwest::Client::new();
-        let resp = client.get(format!("https://api.twitch.tv/helix/users?login={}", &login).as_str())
+        let resp = client
+            .get(format!("https://api.twitch.tv/helix/users?login={}", &login).as_str())
             .bearer_auth(self.twitch_client_token.as_str())
-            .send().await?;
-    
+            .send()
+            .await?;
         let text = resp.text().await?;
         let data = self.remove_data_scope(&text.as_str()).unwrap();
         let user: Vec<User> = serde_json::from_str(&data).unwrap();
         Ok(user)
     }
-    async fn get_user( &self, uid: u32 ) -> Result<Vec<User>, reqwest::Error> {    
+    async fn get_user(&self, uid: u32) -> Result<Vec<User>, reqwest::Error> {
         let client = reqwest::Client::new();
-        let resp = client.get(format!("https://api.twitch.tv/helix/users?id={}", &uid).as_str())
+        let resp = client
+            .get(format!("https://api.twitch.tv/helix/users?id={}", &uid).as_str())
             .bearer_auth(self.twitch_client_token.as_str())
-            .send().await?;
-    
+            .send()
+            .await?;
         let text = resp.text().await?;
         let data = self.remove_data_scope(&text.as_str()).unwrap();
         let user: Vec<User> = serde_json::from_str(&data).unwrap();
         Ok(user)
+    }
+    async fn get_top_games(&self) -> Result<Vec<TopGames>, reqwest::Error> {
+        let client = reqwest::Client::new();
+        let resp = client
+            .get("https://api.twitch.tv/helix/games/top")
+            .header("Client-ID", self.twitch_client_id.as_str())
+            .send()
+            .await?;
+        let text = resp.text().await?;
+        let data = self.remove_data_scope(&text.as_str()).unwrap();
+        let top_games: Vec<TopGames> = serde_json::from_str(&data).unwrap();
+        Ok(top_games)
     }
 }
 
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error>  {
+async fn main() -> Result<(), reqwest::Error> {
     dotenv().ok();
     let twitch_client_id = dotenv::var("TWITCH_CLIENT_ID").unwrap();
     let twitch_client_secret = dotenv::var("TWITCH_CLIENT_SECRET").unwrap();
     let twitch_client_token = dotenv::var("TWITCH_CLIENT_TOKEN").unwrap();
 
     let args: Vec<String> = env::args().collect();
-    let cli = Cli { msg: String::from(args[0].trim()), params: args, twitch_client_id, twitch_client_secret, twitch_client_token };
-    
+    let cli = Cli {
+        msg: String::from(args[0].trim()),
+        params: args,
+        twitch_client_id,
+        twitch_client_secret,
+        twitch_client_token,
+    };
     for param in &cli.params {
         let full_param: Vec<&str> = param.split("=").collect();
         match full_param[0] {
-            "info-user"     => {
+            "info-user" => {
                 let uid: u32 = full_param[1].parse().unwrap();
                 let user = cli.get_user(uid).await?;
                 println!("{:?}", &user);
-            },
+            }
             "isonlive-user" => {
                 let uid: u32 = full_param[1].parse().unwrap();
                 let stream = cli.get_stream_user(uid).await?;
                 println!("{:?}", &stream);
-            },
-            "token"         => {
+            }
+            "token" => {
                 let token = cli.get_token().await?;
                 println!("{}", &token);
-            },
-            "uid"           => {
+            }
+            "uid" => {
                 let login: String = full_param[1].parse().unwrap();
                 let user: Vec<User> = cli.get_user_by_login(login).await?;
                 let uid: &User = user.first().unwrap();
                 println!("{:?}", &uid.id);
-            },
-            "help"          => {
+            }
+            "topgames" => {
+                let top_games = cli.get_top_games().await?;
+                println!("{:?}", top_games);
+            }
+            "topgame" => {
+                let top_games = cli.get_top_games().await?;
+                let first_game: &TopGames = &top_games.first().unwrap();
+                println!("{:?}", first_game);
+            }
+            "help" => {
                 println!("Please, see the readme file for help.");
-            },
-            _               => { }
+            }
+            _ => {}
         };
     }
 
-    if cli.params.len() <= 1 { println!("Don't forget to give me some parameters.."); }
+    if cli.params.len() <= 1 {
+        println!("Don't forget to give me some parameters..");
+    }
 
     Ok(())
 }
